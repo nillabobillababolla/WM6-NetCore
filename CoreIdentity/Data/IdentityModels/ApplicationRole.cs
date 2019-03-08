50c4a30b6beff35bfa332f1ca4f2c83036812f57
Samet Alabey <alabeysamet@gmail.com>
2019-03-08 09:19:10 +0300

    Core Identity Giriş

---
+++ CoreIdentity/Data/IdentityModels/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreIdentity.Data.IdentityModels
+{
+    public class ApplicationRole : IdentityRole
+    {
+        [StringLength(50)]
+        public string Description { get; set; }
+    }
+}
